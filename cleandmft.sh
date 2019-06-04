e0143d3a411098a24072c929901d1a3271bf8e23
uthpalah <ukh0001@mix.wvu.edu>
2019-06-03 20:39:57 -0400

    Updated cleandmft.sh

--- cleandmft.sh
+++ cleandmft.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
-rm -rf actqmc.cix atom_d* fort.20 VASP_mu.out CHG CONTCAR ENERGY_AVG Delta1.inp DMFT_mu.out DMINFO DOSCAR Ed.out EIGENVAL ENERGY Gf1.out G_loc.out IBZKPT imp* info_atom_d.dat ITERINFO ksum* OSZICAR* OUTCAR* PCDAT REPORT Sig* TIMEINFO Trans* UC* vasp* VASP.input WAVECAR XDATCAR wannier* A_k.eps CHGCAR* dos* Gc gt* sig* OUTPUT.*
+rm -rf actqmc.cix atom_d* fort.20 VASP_mu.out CHG CONTCAR ENERGY_AVG Delta1.inp DMINFO DOSCAR Ed.out EIGENVAL ENERGY Gf1.out G_loc.out IBZKPT imp* info_atom_d.dat ITERINFO ksum* OSZICAR* OUTCAR* PCDAT REPORT Sig* TIMEINFO Trans* UC* vasp* VASP.input WAVECAR XDATCAR wannier* A_k.eps CHGCAR* dos* Gc gt* OUTPUT.* INFO_* dmft_params.dat
 
