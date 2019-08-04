ce6328fb59bfc268f6333fc21fbdb76cc22d4ce2
Uthpala Herath <ukh0001@mix.wvu.edu>
2019-08-03 22:20:40 -0400

    cleaned

--- cleanvasp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#A simple script to clean vasp output files
-
-rm -rf CHG* CONTCAR DOSCAR EIGENVAL IBZKPT OSZICAR OUTCAR PCDAT PROCAR REPORT vasprun.xml WAVECAR XDATCAR wannier90* vasp.error vasp.out
