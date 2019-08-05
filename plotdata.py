6a089f968a8b71063e2165d759150e69fbf5200f
Uthpala Herath <ukh0001@mix.wvu.edu>
2019-08-05 11:45:36 -0400

    updated plotdata.py

--- plotdata.py
+++ plotdata.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
 import numpy as np
-import matplotlit.pyplot as plt
-import os
+import matplotlib.pyplot as plt
+import os,sys
 
 infile = sys.argv[1]
-column_number = sys.argv[2]
+column_number = int(sys.argv[2])
 
 
-file = open(infile,’r’)
+file = open(infile,'r')
 data = file.readlines()
 file.close()
 
 
 ydata = [ ]
 for i in range(len(data)):
-    ydata.append(data[i].split()[column_number])
+    ydata.append(float(data[i].split()[column_number]))
 
 
 plt.plot(ydata)
